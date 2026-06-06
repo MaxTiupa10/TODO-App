12235e1d3c5c3521bb9b81431e5ff34556e44398
Maksym <maksimtupa3@gmail.com>
2026-06-06 13:34:34 +0300

    feat: implement task repository, service, and DTO layers with pagination

---
+++ TODO-App.Services/Interfaces/ITaskService.cs
@@ -0,0 +1,8 @@
+﻿using TODO_App.Services.DTOs;
+
+namespace TODO_App.DataAccess.Interfaces;
+
+public interface ITaskService
+{
+    Task<IEnumerable<TaskDto>> GetUserTasksAsync(int userId, int pageNumber, int pageSize, int? categoryId, string? searchQuery);
+}
